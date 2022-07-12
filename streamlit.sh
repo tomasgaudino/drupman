b2a9697affcf76c67d80a86383353b7bd5255e10
tomasgaudino <69804854+tomasgaudino@users.noreply.github.com>
2022-07-12 01:19:40 -0300

    Update streamlit.sh

--- streamlit.sh
+++ streamlit.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/bash
+
+echo "🕛 Updating and upgrading software..."
+sudo apt-get update && sudo apt-get upgrade >/dev/null
+echo "✅ Software updated and upgraded."
+
 # Install python venv
-sudo apt install python3-pip
+echo "🕛 Installing python..."
+sudo apt install python3-pip > /dev/null
+echo "✅ Python installed."
+echo ""
+
 
 #Install pipenv (pip3 install pipenv not working)
+echo "🕛 Installing pipenv..."
 sudo apt install pipenv
+echo "✅ Pipenv installed."
+echo ""
 
 # Create and activate new environment
+echo "🕛 Creating virtual environment..."
 mkdir hummingbots && cd hummingbots
 pipenv shell
+echo "✅ Virtual environment created."
 
 # Expected output >> "Successfully created virtual environment!"
 
 # Install streamlit
+echo "🕛 Installing streamlit and openpyxl..."
 pip install pipenv --upgrade
 pipenv install streamlit
 pipenv install openpyxl
+echo "✅ Streamlit and openpyxl installed."
 
+echo "🕛 Cloning Hummingbots streamlit repo..."
 git clone https://github.com/tomasgaudino/streambot.git
+echo "✅ Virtual environment created."
 
+echo "🕛 Running streamlit..."
 python3 -c streamlit run Home.py
